71ca4aa21751d2452b9b8dbe42f4f7dc13de212a
andela-ajamiu <abdulmujeeb.jamiu@andela.com>
2016-08-28 08:54:25 +0100

    [#128719505] feat(user): add basic user reg/login with email and password

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,27 @@
+class RegistrationsController < Devise::RegistrationsController
+  private
+  def sign_up_params
+    params.require(:user).permit(
+      :firstname,
+      :lastname,
+      :email,
+      :gender,
+      :phone_number,
+      :password,
+      :password_confirmation
+      )
+  end
+
+  def account_update_params
+    params.require(:user).permit(
+      :firstname,
+      :lastname,
+      :email,
+      :gender,
+      :phone_number,
+      :current_password,
+      :password,
+      :password_confirmation
+      )
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email "mujeebjamiu@yahoo.com"
+    password "asdfghjkl"
+  end
+end
