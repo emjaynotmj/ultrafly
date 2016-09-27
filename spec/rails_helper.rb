17af751b43a746afde777ddddac281d89f23db92
andela-ajamiu <abdulmujeeb.jamiu@andela.com>
2016-09-27 10:37:29 +0100

    [#131154247] chore(style): add more corrections with rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,14 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'spec_helper'
-require 'rspec/rails'
-require 'capybara/rails'
-require 'capybara/rspec'
-require 'database_cleaner'
-require 'capybara/poltergeist'
+require "spec_helper"
+require "rspec/rails"
+require "capybara/rails"
+require "capybara/rspec"
+require "database_cleaner"
+require "capybara/poltergeist"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -67,7 +67,7 @@ RSpec.configure do |config|
 end
 
 Capybara.javascript_driver = :poltergeist
-options = {js_errors: false}
+options = { js_errors: false }
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, options)
 end
