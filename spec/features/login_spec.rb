3e493b80a3b5fed72074f59a66470792a813c7a9
andela-ajamiu <abdulmujeeb.jamiu@andela.com>
2016-10-02 16:06:07 +0100

    [#131425913 chore(spec): add more spec

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,14 +1,14 @@
-# require "rails_helper"
+require "rails_helper"
 
-# RSpec.feature "User log in" do
-#   let(:user) { create(:user) }
+RSpec.feature "User log in" do
+  let(:user) { create(:user) }
 
-#   scenario "should allow a registered user to login", js:true do
-#     visit root_path
-#     click_link "Sign In"
-#     fill_in "user_email", with: user.email
-#     fill_in "user_password", with: user.password
-#     click_button "Log in"
-#     expect(page).to have_content "Signed in successfully"
-#   end
-# end
+  scenario "should allow a registered user to login", js:true do
+    visit root_path
+    click_link "Sign In"
+    fill_in "user_email", with: user.email
+    fill_in "user_password", with: user.password
+    click_button "Log in"
+    expect(page).to have_content "Signed in successfully"
+  end
+end
