0e41f3149a39c8e29453240196fa14298890af75
andela-ajamiu <abdulmujeeb.jamiu@andela.com>
2016-10-02 13:35:58 +0100

    [#131425913 chore(spec): fix paypal ssl issue#5

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,14 +7,12 @@ Rails.application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
-  Paypal.sandbox!
+  # Paypal.sandbox!
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
-
-  config.verify_ssl = false
 
   # Configure static file server for tests with Cache-Control for performance.
   config.serve_static_files   = true
